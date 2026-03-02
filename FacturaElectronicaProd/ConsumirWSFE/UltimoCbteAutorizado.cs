cf59f56eb4224edec98bdff1e9f464e782d336cd
Federico Liborio <federico-l-@hotmail.com>
2026-03-02 17:32:31 -0300

    Factura con WEB SERVICE AFIP

--- FacturaElectronicaProd/ConsumirWSFE/UltimoCbteAutorizado.cs
+++ FacturaElectronicaProd/ConsumirWSFE/UltimoCbteAutorizado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.ServiceModel;
 using FacturaElectronicaProd.ConsumirWSFE.ClasesResponse;
 using FacturaElectronicaProd.ServiceReference2;
 using Microsoft.Extensions.Configuration;
@@ -8,35 +9,46 @@ namespace FacturaElectronicaProd.ConsumirWSFE
 {
     public class UltimoCbteAutorizado
     {
-        private readonly string strUrlWsfev1 =
-            (System.Configuration.ConfigurationManager.AppSettings["AFIP"] == "P"
-                ? "https://servicios1.afip.gov.ar/wsfev1/service.asmx"
-                : "https://wswhomo.afip.gov.ar/wsfev1/service.asmx");
+        private readonly IConfiguration _configuration;
+        private readonly string ambiente;
+        private readonly string strUrlWsfev1;
 
-        private ServiceSoapClient servicio;
         private readonly TicketAcceso acceso;
+        private ServiceSoapClient servicio;
 
         public UltimoCbteAutorizado(IConfiguration configuration)
         {
+            _configuration = configuration;
+
+            ambiente = _configuration.GetValue<string>("AFIP");
+
+            strUrlWsfev1 = ambiente == "P"
+                ? "https://servicios1.afip.gov.ar/wsfev1/service.asmx"
+                : "https://wswhomo.afip.gov.ar/wsfev1/service.asmx";
+
             acceso = new TicketAcceso(configuration);
         }
 
-        public CbteAutResponse obtenerUltimoCbtAut(
+        public CbteAutResponse ObtenerUltimoCbtAut(
             int ptoVta,
             int cbteTipo,
             string pathXml = null,
             string pathCertificado = null)
         {
-            // 🔐 auth
-            FEAuthRequest auth = acceso.ObtenerCredencialesTA(pathXml, pathCertificado);
+            // 🔐 Autenticación
+            var auth = acceso.ObtenerCredencialesTA(pathXml, pathCertificado);
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
+            // 🧩 Cliente WCF
+            var endpoint = new EndpointAddress(strUrlWsfev1);
+
             servicio = new ServiceSoapClient(
                 ServiceSoapClient.EndpointConfiguration.ServiceSoap,
-                strUrlWsfev1
+                endpoint
             );
 
+            // 📡 Llamada al WS
             var response = servicio
                 .FECompUltimoAutorizadoAsync(auth, ptoVta, cbteTipo)
                 .GetAwaiter()
@@ -44,6 +56,7 @@ namespace FacturaElectronicaProd.ConsumirWSFE
 
             var result = response.Body.FECompUltimoAutorizadoResult;
 
+            // 📦 Mapeo DTO propio
             var respuesta = new CbteAutResponse
             {
                 cbteNro = result.CbteNro,
@@ -55,16 +68,22 @@ namespace FacturaElectronicaProd.ConsumirWSFE
             {
                 respuesta.errores = Array.ConvertAll(
                     result.Errors,
-                    e => new ErroresResponse { code = e.Code, msg = e.Msg }
-                );
+                    e => new ErroresResponse
+                    {
+                        code = e.Code,
+                        msg = e.Msg
+                    });
             }
 
             if (result.Events != null)
             {
                 respuesta.eventos = Array.ConvertAll(
                     result.Events,
-                    e => new EventosResponse { code = e.Code, msg = e.Msg }
-                );
+                    e => new EventosResponse
+                    {
+                        code = e.Code,
+                        msg = e.Msg
+                    });
             }
 
             return respuesta;
